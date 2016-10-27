e7840259f31501166ef333aed8b6a16329931cfd
Lab 204 Machine <lab204@CS.MERCER.EDU>
2016-10-27 19:34:45 -0400

    Add Trig.java

---
+++ Trig/Trig.java
@@ -0,0 +1,23 @@
+pubic class Trig{
+	public static double findLength(double A, double a, double B)
+	{
+		/* Law of Sines:
+		sin(A)/a = sin(B)/b
+		returns b
+		*/
+		return Math.sin(B)/(Math.sin(A)/a);
+	}
+	public static double findAngle(double A, double a, double b)
+	{
+		//Law of Sines; returns B
+		return Math.asin(b*Math.sin(A)/a);
+	}
+	public static double lawCosines(double b, double c, double A)
+	{
+		/* Law of Cosines
+		a^2 = b^2 + c^2 - b*c*cos(A)
+		returns a
+		*/
+		return Math.sqrt((b*b) + (c*c) - (2*b*c*Math.cos(A));
+	}
+}
