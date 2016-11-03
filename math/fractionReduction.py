1fc1be31a0a6abefbd547bc409bfe6ca879b7a6d
Tyler Burnham <tylerburnham42@gmail.com>
2016-11-02 20:08:22 -0400

    Added Fraction Reduction

---
+++ math/fractionReduction.py
@@ -0,0 +1,17 @@
+from fractions import Fraction
+print(Fraction(16, -10))
+#-8/5
+print(Fraction(123))
+#123
+print(Fraction())
+#0
+print(Fraction('3/7'))
+#3/7
+print(Fraction(' -3/7 '))
+#-3/7
+print(Fraction('1.414213 \t\n'))
+#1414213/1000000
+print(Fraction('-.125'))
+#-1/8
+print(Fraction('7e-6'))
+#7/1000000
